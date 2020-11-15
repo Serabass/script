ca7afe1860c5de552252b37c53916e0e8749630e
Serabass <serabass565@gmail.com>
2020-11-15 23:31:05 +0600

    upd

--- avito.user.js
+++ avito.user.js
@@ -2,21 +2,23 @@
 // @name         Avito prices
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description
-// @author       Serabass
+// @description  try to take over the world!
+// @author       You
 // @match        https://www.avito.ru/*
 // @grant        none
 // ==/UserScript==
 
+let serverRoot = 'http://pp.serabass.net/avito';
+
 function delay(time) {
   return new Promise((resolve) => {
-    setTimeout(resolve, time);
+      setTimeout(resolve, time);
   });
-} 
+}
 
 function setPrice(price) {
   document.querySelector('input#price').value = price.toString();
-  document.querySelector('input[name=price][type=hidden]').value = price.toString();  
+  document.querySelector('input[name=price][type=hidden]').value = price.toString();
 }
 
 function selectFree() {
@@ -25,68 +27,78 @@ function selectFree() {
 
 function clickSubmitButton() {
   let btn = document
-    .querySelector('[class^=layout-footer] > div > div > div:nth-child(2) > button');
+  .querySelector('[class^=layout-footer] > div > div > div:nth-child(2) > button');
   console.log('btn', btn);
   btn.click();
 }
 
-function clickSubmitButton2() {
+async function clickSubmitButton2() {
   let btn = document
-    .querySelector('[class^=layout-footer] > div > div > div:nth-child(2) > button');
+  .querySelector('[class^=layout-footer] > div > div > div:nth-child(2) > button');
   console.log('btn', btn);
+  await delay(2000);
   btn.click();
+}
+
+function clickCheckboxs() {
+  document.querySelectorAll('[class^=service-service] [class^=service-info] label[class^=checkbox-checkbox]')[0].click();
+  document.querySelectorAll('[class^=service-service] [class^=service-info] label[class^=checkbox-checkbox]')[1].click();
 }
 
 function getId() {
   return document.querySelector('input[name="itemId"]').value;
 }
 
+async function getFile() {
+  let res = await fetch(serverRoot + '/file.php');
+  let text = await res.text();
+  return text.trim();
+}
+
 (async function() {
   'use strict';
 
   async function getPrices() {
-    let links = `
-    2032070576 8000
-    `.trim();
-
-    return links
-      .split(/[\r\n]+/)
-      .map(line => line
-        .trim()
-        .split(/\s+/)
-        .map(chunk => chunk.trim())
-      );
+      let file = await getFile();
+      return file
+          .split(/[\r\n]+/)
+          .map(line => line
+               .trim()
+               .split(/\s+/)
+               .map(chunk => chunk.trim())
+              );
   }
 
-  await delay(2000);
+  await delay(5000);
 
   let prices = await getPrices();
   let url = new URL(location);
   let draftId = getId();
 
   if (!draftId) {
-    console.error('No draft id');
-    return;
+      console.error('No draft id');
+      return;
   }
 
   let priceLineIndex = prices.findIndex(([id, price]) => draftId === id);
 
   if (priceLineIndex == -1) {
-    console.error('No price line');
-    return;
+      console.error('No price line');
+      return;
   }
 
   let [id, price] = prices[priceLineIndex];
 
-  await delay(2000);
+  await delay(5000);
 
   setPrice(price);
   selectFree();
-  delay(2000);
+  await delay(5000);
   clickSubmitButton();
-  delay(2000);
-  clickSubmitButton2();
-
+  await delay(15000);
+  clickCheckboxs();
+  setPrice(price);
+  await clickSubmitButton2();
 
   //let next = prices[priceLineIndex + 1];
 
