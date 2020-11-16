4009518313d786d04b67498e1ad001123678256a
Serabass <serabass565@gmail.com>
2020-11-17 00:12:14 +0600

    upd

--- avito2.user.js
+++ avito2.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         Avito prices
+// @name         Avito prices sandbox
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
